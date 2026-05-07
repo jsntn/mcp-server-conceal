3fca7d3bb2597b3bd7902a38d0c86988e44f71be
Jason TIAN <1805410+jsntn@users.noreply.github.com>
2026-05-07 16:45:31 +0800

    Add flake.nix for nix run support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "MCP server that pseudo-anonymizes PII";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "mcp-server-conceal";
+          version = "0.2.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+          # openssl-sys and rusqlite are vendored via cargo features
+          OPENSSL_NO_VENDOR = 0;
+        };
+      }
+    );
+}
